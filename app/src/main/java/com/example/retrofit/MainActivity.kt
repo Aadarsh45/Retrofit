b59a221c62c72a5d087311a14341ddfa3c8c77ae
Aadarsh_45 <aadarshchaurasia45@gmail.com>
2024-12-29 05:17:10 +0530

    @url Manipulation @Path @Query And @QueryMap

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -1,69 +1,82 @@
 package com.example.retrofit
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Observer
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.retrofit.adapter.PostAdapter
 import com.example.retrofit.databinding.ActivityMainBinding
 import com.example.retrofit.repository.Repository
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
+    private val myAdapter by lazy { PostAdapter() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupRecyclerview()
 
 
         val repository = Repository()
         val viewModeFactory = MainViewModeFactory(repository)
         val viewModel = ViewModelProvider(this, viewModeFactory).get(MainViewModel::class.java)
 
-
         val options: MutableMap<String, String> = mutableMapOf()
         options["_sort"] = "id"
         options["_order"] = "desc"
 
+        viewModel.getPost4(2, options)
+        viewModel.myResponse4.observe(this,  {
+            response->
+            if(response.isSuccessful) {
+                response.body()?.let { myAdapter.setData(it) }
 
+            }
+            else{
+                Toast.makeText(this, response.code(), Toast.LENGTH_SHORT).show()
+            }
 
-
-        binding.button.setOnClickListener {
-            val myNumber = Integer.parseInt(binding.edtText.text.toString())
-            viewModel.getPost4(myNumber,options)
-
-            viewModel.myResponse4.observe(
-                this, Observer {
-                        response->
-                    if(response.isSuccessful) {
-
-                        binding.textView.text = response.body().toString()
-                        response.body()?.forEach {
-                            Log.d("Response", it.id.toString())
-                            Log.d("Response", it.title.toString())
-                            Log.d("Response", it.userId.toString())
-                            Log.d("Response", it.body.toString())
-                            Log.d("Response", "----------------------------------------------------------------")
-                        }
-
-
-                    }
-
-                }
-            )
-        }
-
-
-
-
-
+        })
 
 
 
     }
+    private fun setupRecyclerview() {
+        binding.recyclerView.adapter = myAdapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+    }
 }
+
+
+
+
+
+//        binding.button.setOnClickListener {
+//            val myNumber = Integer.parseInt(binding.edtText.text.toString())
+//            viewModel.getPost4(myNumber,options)
+//
+//            viewModel.myResponse4.observe(
+//                this, Observer {
+//                        response->
+//                    if(response.isSuccessful) {
+//
+//                        binding.textView.text = response.body().toString()
+//                        response.body()?.forEach {
+//                            Log.d("Response", it.id.toString())
+//                            Log.d("Response", it.title.toString())
+//                            Log.d("Response", it.userId.toString())
+//                            Log.d("Response", it.body.toString())
+//                            Log.d("Response", "----------------------------------------------------------------")
+//                        }
+//
+//
+//                    }
+//
+//                }
+//            )
+//        }
